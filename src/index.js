d1a097f8e9d42a2990e43c16e667b697f1855b14
Arguello Rincon <CamiloAndres.ArguelloRincon@cencosud.com.co>
2018-12-05 12:09:16 -0500

    Ajustes en estilos de cerrar y puntos de quiebre slick carousel

--- src/index.js
+++ src/index.js
@@ -1,33 +1,51 @@
 require("./index.css");
 
 $(function() {
-  
   // Counter for load more products - Start with 20 by is the default render items
   var cont = 20;
   var numberCollection = 698;
-  
-  $('.toys-brand').slick({
+
+  $(".toys-brand").slick({
     infinite: true,
     slidesToShow: 8,
     slidesToScroll: 8,
+    autoplay: true,
+    autoplaySpeed: 2000,
     centerMode: true,
     responsive: [
       {
-      breakpoint: 1200,
-      settings: {
-        slidesToShow: 5,
-        slidesToScroll: 5,
-        infinite: true,
+        breakpoint: 1300,
+        settings: {
+          slidesToShow: 6,
+          slidesToScroll: 6,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 760,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+          infinite: true
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true
+        }
       }
-    },
-    {
-      breakpoint: 760,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-      }
-    }]
+    ]
   });
 
   // Slider
@@ -42,114 +60,107 @@ $(function() {
       auto: true
     });
   }*/
-  
+
   var loader = $(elem.shelfLoader);
   loader.fadeIn(250);
 
-  getCollectionByNumber(numberCollection,20);
-  
-  $(document).on("click", elem.searchListFiltersSlideControls, function () {
+  getCollectionByNumber(numberCollection, 20);
+
+  $(document).on("click", elem.searchListFiltersSlideControls, function() {
     let t = $(this);
     let sliderWrapper = $(elem.searchListFilters);
-    let scrollWidth = sliderWrapper[0].scrollWidth;
     let slideScrollLeft = sliderWrapper.scrollLeft();
     let delta = 130;
     let newScrollLeft = 0;
-    
+
     if (t.hasClass("prev")) {
       newScrollLeft = slideScrollLeft - delta;
     }
     if (t.hasClass("next")) {
       newScrollLeft = slideScrollLeft + delta;
     }
-    
+
     sliderWrapper.animate({
-      "scrollLeft": newScrollLeft
+      scrollLeft: newScrollLeft
     });
   });
-  
-  
+
   if (Aurora.isMobile()) {
-    $('.search-list-filters').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-     centerMode: true,
-  	});
-        
+    $(".search-list-filters").slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      centerMode: true
+    });
   }
-  
-  $(".adaptive-image").on("click",function(e){
+
+  $("#banner-with-popup-instructions .adaptive-image").on("click", function(e) {
     $("#instructions-to-buy").show();
-  })
-  
+  });
+
   $("#instructions-to-buy button").on("click", function(e) {
-  	$("#instructions-to-buy").hide();
-  })
- 
-  
+    $("#instructions-to-buy").hide();
+  });
+
   // Handle events for Brands
-  $(".container-brand-toys .slick-track").on('click', function(e) 	{
+  $(".container-brand-toys .slick-track").on("click", function(e) {
     var $element = $(e.target).closest("li");
     cont = 20;
     numberCollection = parseInt($element.attr("collection"));
-    //$(this).addClass("active").siblings().removeClass("active");
     loader.fadeIn(250);
     $(".product-shelf").empty();
-    getCollectionByNumber(numberCollection,20);
+    getCollectionByNumber(numberCollection, 20);
   });
-  
-  
+
   // Handle events for categories
-  $(".shelf-header .search-list-filters").on('click', function(e) 	{
+  $(".shelf-header .search-list-filters").on("click", function(e) {
     var $element = $(e.target).closest("button");
     cont = 20;
     numberCollection = parseInt($element.attr("collection"));
-    $element.addClass("active").siblings().removeClass("active");
+    $element
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
     loader.fadeIn(250);
     $(".product-shelf").empty();
-    getCollectionByNumber(numberCollection,20);
+    getCollectionByNumber(numberCollection, 20);
   });
-  
 
-  function getCollectionByNumber(number, quantity){
+  function getCollectionByNumber(number, quantity) {
     Aurora.getProductShelf(`fq=H:${number}`, 1, quantity, 20)
-    .done(res => {
-      if ( res != "" && !$.isEmptyObject(res) && typeof res.activeElement == "undefined") {
-      	
-        $(".product-shelf").html(res);
-      	loader.fadeOut(250);
-      	
-      	// Add flags
- 		var flagDiscount = $(".discount-percent");
-      
-      	for(var i=0;i<flagDiscount.length;i++){
+      .done(res => {
+        if (
+          res != "" &&
+          !$.isEmptyObject(res) &&
+          typeof res.activeElement == "undefined"
+        ) {
+          $(".product-shelf").html(res);
+          loader.fadeOut(250);
+
+          // Add flags
+          var flagDiscount = $(".discount-percent");
+
+          for (var i = 0; i < flagDiscount.length; i++) {
             var discountPercent = parseFloat(flagDiscount[i].innerText);
-            
+
             if (discountPercent >= 10) {
-				flagDiscount[i].innerText = discountPercent + "%";
-				flagDiscount[i].style.display = "flex";
-  			}
+              flagDiscount[i].innerText = discountPercent + "%";
+              flagDiscount[i].style.display = "flex";
+            }
+          }
         }
-
-      }
-    })
-    .fail(er => console.log("error"));
+      })
+      .fail(er => console.log("error"));
   }
-  
 
-    $(window).scroll(function () {
-      let scrollTop = $(this).scrollTop();
-      
-      if($(window).scrollTop() + $(window).height() > $(document).height() - 1000){
-        cont+=5;
-        loader.fadeIn(250);
-        getCollectionByNumber(numberCollection,cont);
-        
-      }
-    });
-  
-
-  
+  $(window).scroll(function() {
+    if (
+      $(window).scrollTop() + $(window).height() >
+      $(document).height() - 1000
+    ) {
+      cont += 5;
+      loader.fadeIn(250);
+      getCollectionByNumber(numberCollection, cont);
+    }
+  });
 });
-
